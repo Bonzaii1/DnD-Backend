3c5be7e73847f337bc1cfaaee30c7149b82501a1
Matthew Herrera <mattherr23@gmail.com>
2026-06-30 23:44:06 -0500

    - Changed user schema - Added google backend auth

--- routes/dbUser.js
+++ routes/dbUser.js
@@ -21,15 +21,29 @@ userRouter.post('/auth', async (req, res) => {
             idToken: credential,
             audience: process.env.GOOGLE_CLIENT_ID
         })
-
+        const role = 'Pathfinder'
         const payload = ticket.getPayload()
 
-        console.log(payload)
+        const { rows } = await db.query(
+            `insert into public."User"
+                    (google_sub, fname, lname, email, role, active, picture, "churchId", "areaId")
+                    values ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+                    on conflict ("google_sub") do update set
+                        fname = excluded.fname,
+                        lname = excluded.lname,
+                        email = excluded.email,
+                        picture = excluded.picture,
+                        updated_at = NOW()
+                    returning *`,
+            [payload.sub, payload.given_name, payload.family_name, payload.email, role, 1, payload.picture, 1, 1]
+        )
 
-        res.status(200).json({ status: "Success", token: "est" })
+
+        res.status(200).json({ result: "Success", fname: payload.given_name, lname: payload.family_name, email: payload.email, picture: payload.picture })
+
 
     } catch (err) {
-        res.status(401).json({ error: 'Invalid Token' })
+        res.status(401).json({ result: "There was an Internal Error", error_message: err })
     }
 
 })
